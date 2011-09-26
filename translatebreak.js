c8b5957f3d51ee50a85e28ba7291e80a4eb6931e
Tom Parker <tom.p.parker@googlemail.com>
2011-09-26 17:55:38 +0100

    Remove px from requested value string

--- translatebreak.js
+++ translatebreak.js
@@ -5,22 +5,22 @@ var getTranslateParam = function(str, direction){
 	if(str && typeof str === 'string'){
 		switch(direction){
 			case "x":
-				regex = /translate3d\((.*?), \-?[0-9]*px, \-?[0-9]*px\)/
+				regex = /translate3d\((.*?)px, \-?[0-9]*px, \-?[0-9]*px\)/
 				resultArray = str.match(regex);
 				result = resultArray[1];
 				break;
 			case "y":
-				regex = /translate3d\(\-?[0-9]*px, (.*?), \-?[0-9]*px\)/
+				regex = /translate3d\(\-?[0-9]*px, (.*?)px, \-?[0-9]*px\)/
 				resultArray = str.match(regex);
 				result = resultArray[1];				
 				break;
 			case "z":
-				regex = /translate3d\(\-?[0-9]*px, \-?[0-9]*px, (.*?)\)/
+				regex = /translate3d\(\-?[0-9]*px, \-?[0-9]*px, (.*?)px\)/
 				resultArray = str.match(regex);
 				result = resultArray[1];				
 				break;
 			default:
-				regex = /translate3d\((.*?), (.*?), (.*?)\)/
+				regex = /translate3d\((.*?)px, (.*?)px, (.*?)px\)/
 				resultArray = str.match(regex);
 				result = resultArray.slice(1);
 		}
