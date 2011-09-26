b433cc29faedf85af1fa6a6c58eb1a85237e21dc
Tom Parker <tom.p.parker@googlemail.com>
2011-09-26 17:39:12 +0100

    Add function to repo

---
+++ translatebreak.js
@@ -0,0 +1,31 @@
+var getTranslateParam = function(str, direction){
+	var regex,
+		result,
+		resultArray;
+	if(str && typeof str === 'string'){
+		switch(direction){
+			case "x":
+				regex = /translate3d\((.*?), \-?[0-9]*px, \-?[0-9]*px\)/
+				resultArray = str.match(regex);
+				result = resultArray[1];
+				break;
+			case "y":
+				regex = /translate3d\(\-?[0-9]*px, (.*?), \-?[0-9]*px\)/
+				resultArray = str.match(regex);
+				result = resultArray[1];				
+				break;
+			case "z":
+				regex = /translate3d\(\-?[0-9]*px, \-?[0-9]*px, (.*?)\)/
+				resultArray = str.match(regex);
+				result = resultArray[1];				
+				break;
+			default:
+				regex = /translate3d\((.*?), (.*?), (.*?)\)/
+				resultArray = str.match(regex);
+				result = resultArray.slice(1);
+		}
+		return result;
+	} else {
+		throw new Error('No string specified');
+	}
+}
